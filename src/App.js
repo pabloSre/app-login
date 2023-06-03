ae92c3f0c231329509561a470903b326c54a2a1c
Victoria Escalada <viqiescalada@gmail.com>
2023-06-03 13:40:10 -0300

    Lista la lógica de inicio de sesion y creacion de usuario

--- src/App.js
+++ src/App.js
@@ -2,40 +2,12 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Login from './Login';
 import CreateUser from './CreateUser';
-import './App.css';
 import BluetoothForm from './Bluetooth';
 import axios from 'axios';
-import { useEffect, useState } from 'react';
+import './App.css';
 
 
-axios.defaults.baseURL = 'http://localhost:3001'; // Reemplaza con la URL de tu servidor backend
-
-/* const App = () => {
-  const [connected, setConnected] = useState(false);
-
-  useEffect(() => {
-    const checkConnection = async () => {
-      try {
-        await axios.get('/'); // Ruta de prueba en tu backend
-        setConnected(true);
-      } catch (error) {
-        console.error('Error connecting to backend:', error);
-      }
-    };
-
-    checkConnection();
-  }, []);
-
-  return (
-    <div>
-      {connected ? (
-        <p>Connected to backend</p>
-      ) : (
-        <p>Failed to connect to backend</p>
-      )}
-    </div>
-  );
-}; */
+axios.defaults.baseURL = 'http://localhost:3001'; 
 function App() {
   return (
     <Router>
--- src/CreateUser.js
+++ src/CreateUser.js
@@ -1,28 +1,18 @@
 import React, { useState } from 'react';
-import './CreateUser.css';
-import axios from 'axios';
 import { GrMail } from 'react-icons/gr';
-import {BsFillPersonLinesFill} from 'react-icons/bs';
-import {RiLockPasswordFill} from 'react-icons/ri';
-import { Link } from 'react-router-dom';
-import { Redirect } from 'react-router-dom';
+import { BsFillPersonLinesFill } from 'react-icons/bs';
+import { RiLockPasswordFill } from 'react-icons/ri';
+import { Redirect, Link } from 'react-router-dom';
+import axios from 'axios';
+import './CreateUser.css';
 
 function CreateUser() {
-
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [signIn, setSignIn] = useState(false);
-  const [error, setError] = useState('');
-
-
-/*  const sendSubmit = (e) => {
-    e.preventDefault();
-    
-    
-    /* prevent default hace que no se recargue la pagina despues de enviar el form */
-    // Aquí realizar alguna acción con los datos del formulario
-    // como enviarlos a un servidor*/
+  const [errorEmail, setErrorEmail] = useState('');
+  const [errorPassword, setErrorPassword] = useState('');
 
   const sendSubmit = async (e) => {
     e.preventDefault();
@@ -32,65 +22,67 @@ function CreateUser() {
       setSignIn(true);
     } catch (error) {
       console.error('Error during login:', error);
-      setError({
-        messageEmail:'this email already exists',
-        messagePass: 'minimum 8 characters'
-      });
+
+      setErrorEmail('This email already exists');
+      setErrorPassword('Minimum 8 characters');
     }
   };
+  
 
   if (signIn) {
-    return <Redirect to="/login" />;
+    return <Redirect to="/" />;
   }
 
-
-
   return (
-<div className='Container-form'>
-      
-      <form onSubmit={sendSubmit} className='Form-create-user'>
-      <h2 className='Title-create-user'>SING UP</h2>
-        <label className='Label-name-create'>
-        <BsFillPersonLinesFill/>Name:
+    <div className="Container-form">
+      <form onSubmit={sendSubmit} className="Form-create-user">
+        <h2 className="Title-create-user">SIGN UP</h2>
+        <label className="Label-name-create">
+          <BsFillPersonLinesFill />
+          Name:
           <input
             type="text"
             value={name}
             onChange={(e) => setName(e.target.value)}
-            className='Input-name-create'
+            className="Input-name-create"
           />
         </label>
         <br />
-        <label className='Label-email-create'  >
-        <GrMail/>Email: 
+        <label className="Label-email-create">
+          <GrMail />
+          Email:
           <input
             type="email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
-            className='Input-email-create'
+            className="Input-email-create"
           />
         </label>
+        
         <br />
-        <label className='Label-password-create'>
-        <RiLockPasswordFill/>Password:
+        <label className="Label-password-create">
+          <RiLockPasswordFill />
+          Password:
           <input
             type="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
-            className='Input-password-create'
+            className="Input-password-create"
           />
         </label>
+        
         <br />
-        <button type="submit"
-        className='Button-register'
-        >CHECK IN
+        <button type="submit" className="Button-register">
+          CHECK IN
         </button>
-        <p className='Link-login'>¿Do you already have an account? <Link to="/" className='Link-log'>Log in</Link></p>
-        {error && <p>{error.messageEmail}</p>}
-        {error && <p>{error.messagePass}</p>}
+        <p className="Link-login">
+          Do you already have an account? <Link to="/" className="Link-log">Log in</Link>
+        </p>
+        <p className="Error-message">{errorEmail}</p>
+        <p className="Error-message">{errorPassword}</p>
       </form>
     </div>
   );
-  
 }
 
 export default CreateUser;
